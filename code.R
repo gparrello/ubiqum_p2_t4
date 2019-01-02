d17e6e54d9245282fa4f72da3a9997a066556783
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-01-02 12:17:20 +0100

    add first draft of code, add product type list

---
+++ code.R
@@ -0,0 +1,46 @@
+pacman::p_load(
+  "caret",
+  "rbokeh",
+  "arules",
+  "arulesViz"
+)
+
+tr <- read.transactions(
+  "./data/ElectronidexTransactions2017.csv",
+  # format = "basket",
+  sep = ','
+)
+
+pr <- read.csv(
+  "./data/item_list.csv"
+)
+
+itemFrequencyPlot(
+  tr,
+  topN = 10,
+  # support = .2,
+  type = "absolute"
+)
+
+rules <- apriori(
+  tr,
+  parameter = list(
+    supp = .01,
+    conf = .05,
+    minlen = 2
+  )
+)
+
+figure() %>%
+  ly_points(support, lift, data = rules@quality, color = confidence)
+
+plot(
+  head(
+    rules,
+    n = 10,
+    by = "lift"
+  ),
+  method = "graph",
+  interactive = TRUE
+)
+
