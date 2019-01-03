2d4bf983c9b99506a98e6121df169aaca0b3fa31
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-01-03 17:59:10 +0100

    add new dimension table

--- code.R
+++ code.R
@@ -2,7 +2,8 @@ pacman::p_load(
   "caret",
   "rbokeh",
   "arules",
-  "arulesViz"
+  "arulesViz",
+  "reshape2"
 )
 
 tr <- read.transactions(
@@ -10,43 +11,63 @@ tr <- read.transactions(
   # format = "basket",
   sep = ','
 )
+tr <- tr[which(size(tr) != 0)]
+newInfo <- data.frame(itemInfo(tr)$labels)
+colnames(newInfo) <- "labels"
 
 pr <- read.csv(
-  "./data/item_list.csv"
+  "./data/item_list2.csv"
+)
+newInfo <- merge(newInfo, pr, by.x = "labels", by.y = "Product", all.x=TRUE)
+itemInfo(tr) <- subset(
+  newInfo,
+  select = c("labels", "Type", "NewType")
 )
 
-itemFrequencyPlot(
+freqPlot <- itemFrequencyPlot(
   tr,
   topN = 10,
   # support = .2,
   type = "absolute"
 )
 
-rules <- apriori(
-  tr,
-  parameter = list(
-    supp = .01,
-    conf = .05,
-    minlen = 2
-  )
-)
+# b2bRules <- apriori(
+#   tr,
+#   parameter = list(
+#     support = .023,
+#     confidence = .3,
+#     minlen = 3
+#   )
+# )
 
-figure() %>%
-  ly_points(support, lift, data = rules@quality, color = confidence)
+# figure() %>%
+  # ly_points(support, lift, data = rules@quality, color = confidence)
 
-plot(
-  head(
-    rules,
-    n = 10,
-    by = "lift"
-  ),
-  method = "graph",
-  interactive = TRUE
-)
+# plot(
+#   head(
+#     rules,
+#     n = 10,
+#     by = "lift"
+#   ),
+#   method = "graph",
+#   interactive = TRUE
+# )
 
 # ruleExplorer(tr)
 # itemInfo(tr) <- data.frame(labels = pr$Product, level1 = pr$Type)
+
 trByProductType <- aggregate(
   tr,
-  pr$Type
+  by = tr@itemInfo[["Type"]],
+  FUN=sum
 )
+
+trByProductType2 <- aggregate(
+  tr,
+  by = tr@itemInfo[["NewType"]]
+)
+
+# trB2C <- subset(tr, (!items %in% lhs(b2bRules) & !items %in% lhs(b2bRules)))
+profileLimit <- 4
+trB2C <- subset(tr, size(tr) < profileLimit)
+trB2B <- subset(tr, size(tr) >= profileLimit)
