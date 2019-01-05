07583cd9450a00380af3627b6d9530c9530c43a4
Gerardo Parrello Brasca <gparrellobrasca+github@gmail.com>
2019-01-05 16:05:16 +0100

    remove unused variable, comment unused code

--- code.R
+++ code.R
@@ -4,24 +4,13 @@ pacman::p_load(
 )
 
 # load other code
-file.sources <- c(
+fileSources <- c(
   "./df.R",
   "./tr.R"
 )
-sapply(file.sources, source, .GlobalEnv)
+sapply(fileSources, source, .GlobalEnv)
+rm(fileSources)
 
 # subset transactions
 b2b <- tr[b2b]
 b2c <- tr[b2c]
-
-# b2bDf <- as.data.frame(b2b)
-# colnames(b2bDf) <- "index"
-# b2cDf <- as.data.frame(b2c)
-# colnames(b2cDf) <- "index"
-# b2bDf$profile <- "b2b"
-# b2cDf$profile <- "b2c"
-# 
-# custProf <- rbind(b2bDf, b2cDf)
-# rownames(custProf) <- custProf$index
-# custProf$index <- NULL
-# itemsetInfo(tr) <- custProf
